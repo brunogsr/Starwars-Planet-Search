d259b93c41f057907a321e544a5db59b18c02542
brunogsr <brunogsr https://github.com/brunogsr>
2023-05-31 17:03:11 -0300

    req 1 finalizado

---
+++ src/components/Table.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react';
+
+function Table() {
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    fetch('https://swapi.dev/api/planets')
+      .then((response) => response.json())
+      .then((data) => {
+        const dataWithoutResidents = data.results.map((planet) => {
+          delete planet.residents;
+          return planet;
+        });
+        setPlanets(dataWithoutResidents);
+      });
+  }, []); // aula do course dia 1 useEffect
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            planets.map((planet) => (
+              <tr key={ planet.name }>
+                <td>{planet.name}</td>
+                <td>{planet.rotation_period}</td>
+                <td>{planet.orbital_period}</td>
+                <td>{planet.diameter}</td>
+                <td>{planet.climate}</td>
+                <td>{planet.gravity}</td>
+                <td>{planet.terrain}</td>
+                <td>{planet.surface_water}</td>
+                <td>{planet.population}</td>
+                <td>{planet.films}</td>
+                <td>{planet.created}</td>
+                <td>{planet.edited}</td>
+                <td>{planet.url}</td>
+              </tr>
+            ))
+          }
+
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Table;
